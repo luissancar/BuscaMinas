e9b4da8d84b501996103c8a5fd39fc87221550c7
luissancar <luissancar@yahoo.es>
2016-11-03 13:10:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/luissancar/buscaminas/Casilla.java
@@ -0,0 +1,24 @@
+package com.example.luissancar.buscaminas;
+
+/**
+ * Created by luissancar on 3/11/16.
+ */
+
+public class Casilla {
+
+    public int x,y,ancho;
+    public int contenido=0;
+    public boolean destapado=false;
+    public void fijarxy(int x,int y, int ancho) {
+        this.x=x;
+        this.y=y;
+        this.ancho=ancho;
+    }
+
+    public boolean dentro(int xx,int yy) {
+        if (xx>=this.x && xx<=this.x+ancho && yy>=this.y && yy<=this.y+ancho)
+            return true;
+        else
+            return false;
+    }
+}
